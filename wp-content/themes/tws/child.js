465b5caab7e9b9a212e4d26af74ddd58f853622b
zz853 <86990873+zz853@users.noreply.github.com>
2022-06-08 16:33:21 +0800

    Update child.js

--- wp-content/themes/tws/child.js
+++ wp-content/themes/tws/child.js
@@ -1,14 +1,1 @@
-var siteTitle = document.title;
-$(".entry-content img").each(function () {
-var alt = this.alt;
-var src = this.src;
-if (!alt) {
-$(this).attr("alt", siteTitle);
-}
-$(this).wrap("<a href='"+ src +"' data-fancybox='group' data-caption='"+ siteTitle +"' title='"+ siteTitle +"'></a>");
-});
-$('[data-fancybox]').fancybox({
-buttons: ["slideShow","zoom","fullScreen","thumbs","close"],
-transitionEffect: "zoom-in-out",
-protect: true
-})
+
